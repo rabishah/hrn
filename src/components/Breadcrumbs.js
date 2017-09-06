bf4b5a30a3083cd0f038422bbb8e4ce08e8342b3
Rabi C Shah <green.rabi@hotmail.com>
2017-09-06 21:59:08 +0530

    Adding Breadcrumb component.

---
+++ src/components/Breadcrumbs.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import cx from 'classnames';
+
+import './Breadcrumbs.scss';
+
+function Splitter(s) {
+  console.log('selected', s.selected);
+  const sClass = cx({
+    'splitter': true,
+    'opacity-50': !s.selected
+  });
+
+  return (
+    <span className={sClass}>/</span>
+  )
+}
+
+class Breadcrumbs extends Component {
+  render() {
+    const { list, selected } = this.props;
+
+    const $list = list.map((l, i) => {
+      const lClass = cx({
+        'opacity-50': i !== selected
+      });
+
+      return (
+        <li className="Breadcrumbs__List" key={i + 'breadcrumbs'}>
+          <div className={lClass}>{l}</div>
+          <Splitter selected={i === selected || i  === selected - 1} />
+        </li>
+      )
+    });
+
+    return (
+      <ul className="Breadcrumbs">{$list}</ul>
+    )
+  }
+}
+
+export default Breadcrumbs;
