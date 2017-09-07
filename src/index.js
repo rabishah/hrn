1b98dccec27cc4d42f59d856459252b283b07bef
Rabi C Shah <green.rabi@hotmail.com>
2017-09-07 23:16:21 +0530

    Adding to gh-pages.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './index.css';
-import 'font-awesome/css/font-awesome.css';
-
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
